79ad2f3bb9516f5f41556b916243174044a01c4d
chris thompson <christhompson05@gmail.com>
2019-12-23 19:54:51 -0500

    set up records tab

--- snooker-counter-plus/src/app/records-tab/records-tab.ts
+++ snooker-counter-plus/src/app/records-tab/records-tab.ts
@@ -1,12 +1,71 @@
-import { Component } from '@angular/core';
+import { Component, NgZone, OnInit } from '@angular/core';
+import { PlayerModel } from '../models/player.model';
+import { PlayerService } from '../services/player.service';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'records-tab.html',
   styleUrls: ['records-tab.scss']
 })
-export class RecordsTab {
+export class RecordsTab implements OnInit {
+  players: PlayerModel[] = [];
+  mostWins: PlayerModel[] = [];
+  bestWinPct: PlayerModel[] = [];
+  mostPoints: PlayerModel[] = [];
+  bestPointPerGame: PlayerModel[] = [];
 
-  constructor() {}
+  constructor(
+      private playerService: PlayerService,
+      private zone: NgZone
+  ) {}
 
+  async ngOnInit(): Promise<void> {
+    await this.zone.run(async () => {
+      this.players = await this.playerService.getPlayers();
+      this.getRecords();
+    });
+  }
+
+  private async getRecords(): Promise<void> {
+    this.mostWins = this.getMostWins();
+    this.bestWinPct = this.getBestWinPct();
+    this.mostPoints = this.getMostPoints();
+    this.bestPointPerGame = this.getBestPointsPerGame();
+  }
+
+  private getMostWins() {
+    return this.players.sort((a, b) => {
+      if (a.wins <= b.wins) {
+        return -1;
+      }
+      return 1;
+    });
+  }
+
+  private getBestWinPct() {
+    return this.players.sort((a, b) => {
+      if ((a.wins / a.gamesPlayed) <= (b.wins / b.gamesPlayed)) {
+        return -1;
+      }
+      return 1;
+    });
+  }
+
+  private getMostPoints() {
+    return this.players.sort((a, b) => {
+      if (a.points <= b.points) {
+        return -1;
+      }
+      return 1;
+    });
+  }
+
+  private getBestPointsPerGame() {
+    return this.players.sort((a, b) => {
+      if ((a.points / a.gamesPlayed) <= (b.points / b.gamesPlayed)) {
+        return -1;
+      }
+      return 1;
+    });
+  }
 }
